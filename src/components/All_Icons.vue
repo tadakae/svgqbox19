2e164b143164dc97b225c8077fcda2648b8ca7d1
heisenberg <ramazan.sapar003@gmail.com>
2023-08-28 17:51:25 +0600

    obnova2

--- src/components/All_Icons.vue
+++ src/components/All_Icons.vue
@@ -6,11 +6,15 @@
                type="search"
                placeholder="search"
                v-model="search">
+        <div class="search_svg">
+            <svg width="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 20L15.8033 15.8033C15.8033 15.8033 14 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5C18 11.0137 17.9484 11.5153 17.85 12" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+        </div>
     </div>
+
 
     <div class="icon_box">
         <div class="icons_main" v-for="icon in icons" :key="icon.id">
-            <img :src="icon.url" style="width: 45px; margin: 20px; " alt="">
+            <img :src="icon.url" style="width: 45px; margin: 20px; " alt="" >
         </div>
     </div>
     <Pagination />
@@ -35,7 +39,6 @@ export default {
     components: {Pagination},
 
     data() {
-
 
         return {
             icons: [],
@@ -105,6 +108,12 @@ export default {
     margin: 10px;
     border-radius: 15px;
 
+}
+
+.search_svg {
+    position: absolute;
+    margin-left: 125px;
+    margin-top: -5px;
 }
 
 
